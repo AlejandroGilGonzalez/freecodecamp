3f0897c125864e58ab0d4f82ce8f592f328bc57c
Alejandro Gil Gonzalez <alexalexait98@gmail.com>
2026-03-03 12:59:05 +0100

    Change isinstance with only lists

--- 01_03_2026.py
+++ 01_03_2026.py
@@ -2,10 +2,12 @@
 
 # Given an array, determine if it is flat
 
+# An array is flat if none of its elements are arrays.
+
 def is_flat(arr):
     flat = True
     for i in arr:
-        if isinstance(i,(list,tuple,set)):
+        if isinstance(i,list):
             flat = False
             break
         else:
