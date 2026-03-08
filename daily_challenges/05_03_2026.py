e2cf4158f5da1d4c8a39b710eb3d5286b403d5e3
AlejandroGilGonzalez <alexalexait98@gmail.com>
2026-03-08 12:16:31 +0100

    Uploading more changes

--- daily_challenges/05_03_2026.py
+++ daily_challenges/05_03_2026.py
@@ -11,7 +11,7 @@ def smallest_gap(input: str) -> str:
     # Counts the repeated characters and their index
     for i in range(len(input)):
         char = input[i]
-        if input[i:].count(char) > 1: gap_chars[i] = char
+        if input[i:].count(char) > 1: gap_chars.setdefault(i,char)
     print(gap_chars) 
     
     # Finds the gaps between the repeated characters
