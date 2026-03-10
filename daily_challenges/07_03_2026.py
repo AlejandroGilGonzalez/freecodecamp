691c15ceb96560e315ce18b21ce16fc2e6c6ca3f
AlejandroGilGonzalez <alexalexait98@gmail.com>
2026-03-10 12:00:23 +0100

    Changes

--- daily_challenges/07_03_2026.py
+++ daily_challenges/07_03_2026.py
@@ -1,6 +1,6 @@
 # Daily Challenge 06/03/2026
 
-# Given a window size, the width of an element in viewport width "vw" units,
+# Given a window size, the width of an element in viewport width "vw" units.
 # and the height of an element in viewport height "vh" units, determine the size of the element in pixels.
 
 
---
+++ daily_challenges/08_03_2026.py
@@ -0,0 +1,27 @@
+# Daily Challenge 08/03/2026
+
+#Given a string, determine whether it is a valid CSS hsl() color value.
+
+"""A valid HSL value must be in the format "hsl(h, s%, l%)", where:
+
+h (hue) must be a number between 0 and 360 (inclusive).
+s (saturation) must be a percentage between 0% and 100%.
+l (lightness) must be a percentage between 0% and 100%.
+Spaces are only allowed:
+
+After the opening parenthesis
+Before and/or after the commas
+Before and/or after closing parenthesis
+Optionally, the value can end with a semi-colon (";").
+
+For example, "hsl(240, 50%, 50%)" is a valid HSL value. """
+
+import re
+
+def is_valid_hsl(hsl):
+    # Checks if hsl format given is wether valid or invalid.
+
+    hue = re.findall(r"hsl\b\d+",hsl)
+    print(hue)
+
+    return hsl
