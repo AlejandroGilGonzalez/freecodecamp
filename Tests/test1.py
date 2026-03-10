a56e923a3a92b388834f0b0fb878a484b88bd02f
AlejandroGilGonzalez <alexalexait98@gmail.com>
2026-03-10 11:46:38 +0100

    Daily Challenge 06/03/2026 Completed

--- Tests/test1.py
+++ Tests/test1.py
@@ -1,7 +1,37 @@
-try:
-    goal = input("Your goal int: ")
-    int(goal)
-except ValueError:
-    print("Must be a number")
-else:
-    print(goal)
+from random import randint as ran
+
+
+with open("dics/b.txt","r",encoding="utf-8") as f:
+    lista = list(f)
+
+new_lista = [pal.replace("\n","") for pal in lista]
+dictionary = {i:word for i,word in enumerate(new_lista)}
+
+number = ran(0,len(dictionary))
+
+s_word = dictionary[number]
+s_word = {k:j for k,j in enumerate(s_word)}
+print(s_word)
+
+layout ="".join(["_" for i in range(len(s_word))])
+
+print(layout)
+
+x = False
+count = 0
+while x == False or count < 10:
+    letra = input("Escoja una letra: ")
+    if len(letra)>1:
+        print("No puede ser más de 1 letra.")
+        print(layout)
+    else:
+        count += 1
+        if layout == s_word:
+            x = True
+        if letra in s_word.values():
+            layout = layout[:ind]+letra+layout[ind+1:]
+        print(layout)
+    
+
+
+    
---
+++ daily_challenges/06_03_2026.py
@@ -0,0 +1,23 @@
+# Daily Challenge 06/03/2026
+
+# Given a window size, the width of an element in viewport width "vw" units,
+# and the height of an element in viewport height "vh" units, determine the size of the element in pixels.
+
+
+import re
+
+def get_element_size(window_size: str, element_vw:str, element_vh:str)->str:
+    
+    # Gets the window size as int and takes the vw and vh percentage of it.
+
+    numbers = list(map(int, re.findall(r"\d+",window_size)))
+    element_vw = int(element_vw.replace("vw",""))
+    element_vh = int(element_vh.replace("vh",""))
+    one = int(numbers[0] * (element_vw/100))
+    two = int(numbers[1] * (element_vh/100))
+    window_size = str(one) + " x " + str(two)
+
+    return(window_size)
+
+
+get_element_size("1200 x 800", "50vw", "50vh")
