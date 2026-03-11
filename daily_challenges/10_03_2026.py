1a823048626fac5349d15496b32793f27265e7a5
AlejandroGilGonzalez <alexalexait98@gmail.com>
2026-03-11 10:20:43 +0100

    Daily Challenge 10/03/2026 Completed

--- daily_challenges/10_03_2026.py
+++ daily_challenges/10_03_2026.py
@@ -3,7 +3,7 @@
 # Given an array, a value to insert into the array, and an index to insert the value at,
 # return a new array with the value inserted at the specified index.
 
-def insert_into_array(arr, value, index):
+def insert_into_array(arr:list, value:int, index:int) -> list:
     # Inserts value into an array at specified index
 
     arr.insert(index,value)
