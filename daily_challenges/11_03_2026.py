346bd493bc04d8b16c7034a64a90dd6dafa6559f
AlejandroGilGonzalez <alexalexait98@gmail.com>
2026-03-11 10:34:45 +0100

    Daily Challenge 11/03/2026 Completed

---
+++ daily_challenges/11_03_2026.py
@@ -0,0 +1,20 @@
+# Daily Challenge 11/03/2026
+
+# Given a string of words, return a new string where each word is replaced by its length.
+
+# Words in the given string will be separated by a single space
+# Keep the spaces in the returned string.
+
+def convert_words(words:str) -> str:
+    # Gets the length of each word in a string and returns a string showing those lengths
+
+    separated = words.split()
+
+    for i in range(len(separated)):
+        separated[i] = str(len(separated[i]))
+
+    result = " ".join(separated)
+    
+    return (result)
+
+convert_words("Lorem ipsum dolor sit amet consectetur adipiscing elit donec ut ligula vehicula iaculis orci vel semper nisl")
