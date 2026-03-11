5e766ea482c69105609732a1cc280efcbace551b
AlejandroGilGonzalez <alexalexait98@gmail.com>
2026-03-11 10:11:00 +0100

    09/03/2026 Daily Challenge Completed

---
+++ daily_challenges/09_03_2026.py
@@ -0,0 +1,14 @@
+# 09/03/2026 Daily Challenge
+
+# Given an array of numbers, return the sum of all the numbers.
+
+
+def sum_array(sum_array: list) -> int:
+    # Sums all the numbers in an array.
+
+    result = sum(sum_array)
+
+
+    print(result)
+
+sum_array([203, 145, -129, 6293, 523, -919, 845, 2434])
