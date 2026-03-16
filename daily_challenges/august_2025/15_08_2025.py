ee5801c1813a2502a1a6708586e487145aae1ab7
AlejandroGilGonzalez <alexalexait98@gmail.com>
2026-03-16 09:28:29 +0100

    Uploading Daily Challenge 15/08/2025 Completed

---
+++ daily_challenges/august_2025/15_08_2025.py
@@ -0,0 +1,27 @@
+# 15/08/2025 Daily Challenge
+
+# Given a string, return a jumbled version of that string where each word is transformed using the following constraints:
+"""
+- The first and last letters of the words remain in place
+- All letters between the first and last letter are sorted alphabetically.
+- The input strings will contain no punctuation, and will be entirely lowercase.
+"""
+
+def jbelmu(text):
+
+    text = text.split(" ")
+    
+
+    new_text = []
+    
+    for word in text:
+        if len(word) > 1:
+            sorted_w = sorted(word[1:-1])
+            word = word[0] + "".join(sorted_w) + word[-1]
+            new_text.append(word)
+        else:
+            new_text.append(word)
+
+    return (" ".join(new_text))
+
+jbelmu("i love jumbled text")
