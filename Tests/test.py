53e0ddd4912d3f348608804d4b6254e37ef88055
AlejandroGilGonzalez <alexalexait98@gmail.com>
2026-04-02 13:57:31 +0200

    Uploading Daily Challenge 02/04/2026 Completed

--- Tests/test.py
+++ Tests/test.py
@@ -1,35 +1,37 @@
 from random import randint, sample
 import re
 
-class User:
-    user = ""
-    password = ""
-    
-    def __init__(self, user, password):
-        self.user = user
-        self.password = password
-    
-    def login(self, user, password):
-        print (user, self.user)
-        print (password, self.password)
-    
-
 # Open the dictionary.txt:
 
 with open("dics/dics.txt","r", encoding="utf8") as f:
     dictionary = f.read()
 
-# Create a random username and password for each user:
+# Define a user class:
+ 
+class User:
+    user = ""
+    password = ""
 
-users = ["User1", "User2"]
-for i in range(2):
-    users[i] = User("".join(sample(re.findall(r"[a-zA-Z]+",dictionary), 2)),randint(1000,9999))
+    # Create a random username and password for each user:
 
-print(users[0].login)
-quit()
+    def __init__(self, user, password):
+        self.user = "".join(sample(re.findall(r"[a-zA-Z]+",dictionary), 2))
+        self.password = randint(1000,9999)
+    
+    # Stablish login credentials
+    
+    def login(self, user, password):
+        print (user, self.user)
+        print (password, self.password)
 
-# Create a username using 2 random words:
 
+def main():
 
+    user = User("user","password")
 
-print(user.password)
+    print(user.user)
+    print(user.password)
+
+    return ""
+
+main()
