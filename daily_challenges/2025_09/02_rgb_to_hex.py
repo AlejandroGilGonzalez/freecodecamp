543634467fd9e855dc12bef9bb4f0bea32cf589f
AlejandroGilGonzalez <alexalexait98@gmail.com>
2026-04-02 20:55:56 +0200

    Uploading Daily Challenge 02/09/2025 Completed

---
+++ daily_challenges/2025_09/02_rgb_to_hex.py
@@ -0,0 +1,25 @@
+# 02/09/2025 Daily Challenge
+
+# Given a CSS rgb(r, g, b) color string, return its hexadecimal equivalent.
+#
+# Make any letters lowercase.
+#
+# Return a # followed by six characters. Don't use any shorthand values.
+
+import re
+
+def rgb_to_hex(rgb:str) -> str:
+    
+    # Convert the string to a list of numbers:
+
+    rgb_list = re.findall(r"(\d+)", rgb)
+    rgb = rgb_list
+
+    for i in range(len(rgb)):
+        rgb[i] = int(rgb[i])
+
+    return (f"#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}")
+
+
+
+rgb_to_hex("rgb(255, 255, 255)")
