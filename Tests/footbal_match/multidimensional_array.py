b78bc2b558514d76b9e813cbe8f71892861b5652
AlejandroGilGonzalez <alexalexait98@gmail.com>
2026-04-02 17:54:23 +0200

    Updating folders and names for my tests

---
+++ Tests/footbal_match/multidimensional_array.py
@@ -0,0 +1,31 @@
+# Create a multidimensional array with fake football teams:
+
+from random import randint
+import json
+
+with open('players_array.json', encoding="utf8") as json_file:
+    copas = json.load(json_file)
+
+# Definir el ID y la puntuación de cada jugador:
+
+ide = 1
+for partido, equipos in copas["copa_america"]["fase_grupos"].items():
+    for team, jugadores in equipos.items():
+        for jugador, stats in jugadores.items():
+            stats["Puntuación"] = randint(50,99)
+            json.dump(copas, open("players_array.json", "w", encoding="utf8"))
+            print(jugador,stats)
+ 
+
+# Definir una función que compare las distintas puntuaciones por clase de jugador:        
+
+def lucha (equipo_local:dict, equipo_visitante:dict):
+    
+    for jugadores in equipo_local.values():
+        for jugador, stats in jugadores.items():
+            if stats["Posición"] == equipo_visitante[jugadores][stats]["Posición"]:
+                print(yes)
+
+    return ""
+
+
---
+++ Tests/hangmans/auto_hangman.py
@@ -0,0 +1,42 @@
+import re
+import random
+
+# Get the dictionary from the txt file:
+
+with open("dics/dics.txt","r",encoding="utf-8") as f:
+    dic = f.read()
+
+# Get a random word from the dictionary:
+
+s_word = random.choice(re.findall(r"[a-zA-ZáéíóúÁÉÍÓÚñÑüÜ]+",dic))
+print(s_word)
+
+# Get a list of all the words with the same length as secret word.
+
+words_list = re.findall(layout,dic)
+
+# Print the layout:
+
+layout = "\w" * len(s_word)
+print(layout)
+
+# Find the count of each character to act as probability:
+
+good_chars = "abcdefghijklmnñopqrstuvwxyz"
+good_chars += good_chars.upper() + "áéíóúÁÉÍÓÚñÑüÜ"
+
+probab = {}
+
+for char in dic:
+    if not char in probab.keys():
+        if char in good_chars:
+            probab[char] = dic.count(char)
+
+# Sort characters by probability:
+
+top_chars = dict(sorted(probab.items(),key=lambda item:item[1],reverse=True))
+
+
+
+
+
